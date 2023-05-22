f36acff6419625685823e3716337233290e10056
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2023-05-22 11:31:13 +0200

    05

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,18 +1,26 @@
 <?php
-// src/Controller/ProgramController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
-    public function index(): Response
+    #[Route('/{id}', methods: ['GET'], name: 'program_show')]
+    public function show(int $id): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
-         ]);
+        return $this->render('program/show.html.twig', [
+            'program_id' => $id,
+        ]);
     }
 }
+
+
+
+
+
+
+
